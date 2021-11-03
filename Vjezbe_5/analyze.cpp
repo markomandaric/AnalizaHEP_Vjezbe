11a6aed91309a787722c0b78ade61940a2abf3ed
markomandaric <mmandari1@pmfst.hr>
2021-11-03 13:31:20 +0100

    Vjezbe_5

---
+++ Vjezbe_5/analyze.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <TROOT.h>
+#include <TFile.h>
+#include <TChain.h>
+#include <TApplication.h>
+#include "Analyzer.h"
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <random>
+#include <fstream>
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+#include <TApplication.h>
+using namespace std;
+int main()
+{
+    Analyzer *analysis;
+    analysis=new Analyzer();
+    //analysis->Loop();
+    analysis->PlotHistogram();
+    return 0;
+}
