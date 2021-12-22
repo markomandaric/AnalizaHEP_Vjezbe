e50e9186a488402fb6b7ca1a22ea41f28b4b4dcc
markomandaric <mmandari1@pmfst.hr>
2021-12-22 11:32:00 +0100

    Vjezbe_10

--- Vjezbe_10/Analyzer.cpp
+++ Vjezbe_10/Analyzer.cpp
@@ -15,6 +15,7 @@
 #include <TMath.h>
 #include <TGraphErrors.h>
 #include <TF1.h>
+#include <TLine.h>
 using namespace std;
 
 Analyzer::Analyzer()
@@ -32,42 +33,60 @@ void Analyzer::Draw()
 	Double_t a_error[5]={1.0,1.9,3.1,3.9,5.1};
 	Double_t F[5]={1.0,2.0,3.0,4.0,5.0}; 	
 	Double_t F_error[5]={0.0,0.0,0.0,0.0,0.0};
-	Double_t theta,min,sigma_lower, sigma_upper;
+	Double_t theta,min,sigma_lower, sigma_upper,mass,x1,x2;
 	TF1* fit;
-	fit=new TF1("fit", "1/[0]*x",0,6);
+	fit=new TF1("fit", "1/[0]*x",0,10);
 	fit->SetParameter(0,0.1);
 	fit->SetParName(0,"mass");
 
 	auto gr = new TGraphErrors(5,F,a,F_error,a_error);
 	gr->GetXaxis()->SetTitle("F [N]");
 	gr->GetYaxis()->SetTitle("a [m/s^{2}]");
-	gr->Draw("AP");
 	gr->Fit(fit);
-	
+	gr->Draw("AP");
 	
 	canvas->cd(2);
 
 	TF1* chi_square;
-	chi_square=new TF1("chi_square","(pow(([0]-[1]*x),2)/([2]*[2]))+(pow(([3]-[4]*x),2)/([5]*[5]))+(pow(([6]-[7]*x),2)/([8]*[8]))+(pow(([9]-[10]*x),2)/([11]*[11]))+(pow(([12]-[13]*x),2)/([14]*[14]))",9.5,10.5);
+	chi_square=new TF1("chi_square","(pow(([0]-[1]*x),2)/([2]*[2]))+(pow(([3]-[4]*x),2)/([5]*[5]))+(pow(([6]-[7]*x),2)/([8]*[8]))+(pow(([9]-[10]*x),2)/([11]*[11]))+(pow(([12]-[13]*x),2)/([14]*[14]))",9,11);
 	for(int i=0;i<5*5;i+=3)
 	{
 		chi_square->SetParameter(i,a[i/3]);
 		chi_square->SetParameter(i+1,F[i/3]);
 		chi_square->SetParameter(i+2,a_error[i/3]);
 	}
-
-	theta=chi_square->GetMinimumX(8,11,1.E-10,100,false);
-   	min=chi_square->GetMinimum(8,11,1.E-10,100,false);
-    	sigma_lower=theta-chi_square->GetX(min+1,8,11,1.E-10,100,false); 
-    	sigma_upper=chi_square->GetX(min+1,8,11,1.E-10,100,false)-theta;
 	
-	auto l=new TLine();
+	chi_square->SetLineColor(kCyan);
+    	chi_square->GetYaxis()->SetRangeUser(0, 10.0);
+    	chi_square->SetTitle("Chi-square; #hat{#theta}; #chi^{2}");
 	
 	chi_square->Draw();
-	double m=1/theta;
-	cout << "chi_squared\t\t\t\t" << theta << "\t\t- " << sigma_lower/(theta*theta) << "    + " << sigma_upper/(theta*theta) << endl;	
-	cout<<"m="<<m<<endl;
 
+	min=chi_square->GetMinimum();
+	theta=chi_square->GetMinimumX();
+	mass=1/theta;
+	cout << "mass = " << mass <<endl;
+	
+	x1 = chi_square->GetX(min + 1.0, 6.0, chi_square->GetMinimumX() - 0.0001);
+        x2 = chi_square->GetX(min + 1.0, chi_square->GetMinimumX() + 0.0001, 14.0);
+
+    	sigma_lower = chi_square->GetMinimumX() - x1;
+    	sigma_upper = x2 - chi_square->GetMinimumX();
+    	cout << "Sigma_lower = " << -1.0*(sigma_lower/pow(theta, 2)) <<endl;
+    	cout << "Sigma_upper = " << 1.0*(sigma_upper/pow(theta, 2)) <<endl;
+
+	auto l1 = new TLine(x1, min + 1.0, x2, min + 1.0);
+    	auto l2 = new TLine(theta, 0, theta, min);
+    	auto l3 = new TLine(x1, 0.0, x1, min + 1.0);
+    	auto l4 = new TLine(x2, 0.0, x2, min + 1.0);
+    	l1->SetLineStyle(kDashed);
+    	l2->SetLineStyle(kDashed);
+    	l3->SetLineStyle(kDashed);
+    	l4->SetLineStyle(kDashed);
+    	l1->Draw();
+    	l2->Draw();
+    	l3->Draw();
+    	l4->Draw();
 	
 
 
