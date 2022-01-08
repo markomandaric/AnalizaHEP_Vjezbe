ec9c3852f8ca861a1c944a648cd5c1a73ea1a58c
markomandaric <mmandari1@pmfst.hr>
2022-01-08 13:06:50 +0100

    Vjezbe_7_ROC

--- Vjezbe_7/Analyzer.cpp
+++ Vjezbe_7/Analyzer.cpp
@@ -5,40 +5,18 @@
 #include <TCanvas.h>
 #include <iostream>
 #include <Riostream.h>
+#include <TH1F.h>
+#include <TH2F.h>
 #include <TColor.h>
 #include <TLegend.h>
 #include <TLorentzVector.h>
 #include <vector>
+#include <TGraph.h>
 #include <THStack.h>
 void Analyzer::Loop()
 {
-//   In a ROOT session, you can do:
-//      root> .L Analyzer.C
-//      root> Analyzer t
-//      root> t.GetEntry(12); // Fill t data members with entry number 12
-//      root> t.Show();       // Show values of entry 12
-//      root> t.Show(16);     // Read and show values of entry 16
-//      root> t.Loop();       // Loop on all entries
-//
-
-//     This is the loop skeleton where:
-//    jentry is the global entry number in the chain
-//    ientry is the entry number in the current Tree
-//  Note that the argument to GetEntry must be:
-//    jentry for TChain::GetEntry
-//    ientry for TTree::GetEntry and TBranch::GetEntry
-//
-//       To read only selected branches, Insert statements like:
-// METHOD1:
-//    fChain->SetBranchStatus("*",0);  // disable all branches
-//    fChain->SetBranchStatus("branchname",1);  // activate branchname
-// METHOD2: replace line
-//    fChain->GetEntry(jentry);       //read all branches
-//by  b_branchname->GetEntry(ientry); //read only this branch
-   if (fChain == 0) return;
-
+	  if (fChain == 0) return;
    Long64_t nentries = fChain->GetEntriesFast();
-
    Long64_t nbytes = 0, nb = 0;
    for (Long64_t jentry=0; jentry<nentries;jentry++) {
       Long64_t ientry = LoadTree(jentry);
@@ -47,17 +25,14 @@ void Analyzer::Loop()
       // if (Cut(ientry) < 0) continue;
    }
 }
-
 void Analyzer::Plot(TString Path)
 {
-
-
+	
         TFile* f = new TFile("/home/public/data/"+Path+"/ZZ4lAnalysis.root");
         TTree* tree = (TTree*) f->Get("ZZTree/candTree");
         Init(tree);
    	TH1F* histoCounter=(TH1F*)f->Get("ZZTree/Counters");
 	float binContent=histoCounter->GetBinContent(40);
-	
 
 	TLorentzVector* higgs = new TLorentzVector();
         TLorentzVector* Z1 = new TLorentzVector();
@@ -67,64 +42,117 @@ void Analyzer::Plot(TString Path)
         TLorentzVector* l3 = new TLorentzVector();
         TLorentzVector* l4 = new TLorentzVector();
 
+	float signalDis, backgroundDis;
+	float co=70.0;	
+
 	TCanvas* c=new TCanvas("c","c",1000,1000);
-	THStack* hs=new THStack("hs","Reconstructed mass");
+	c->Divide(2,2);
+	TGraph *ROC = new TGraph();
 
 	  if (fChain == 0) return;
 
    Long64_t nentries = fChain->GetEntriesFast();
-
    Long64_t nbytes = 0, nb = 0;
    for (Long64_t jentry=0; jentry<nentries;jentry++) {
       Long64_t ientry = LoadTree(jentry);
       if (ientry < 0) break;
       nb = fChain->GetEntry(jentry);   nbytes += nb;
-
+       
 
 	float w=(137.0*1000.0*xsec*overallEventWeight)/binContent;
-
-	
-
+ 
 	l1->SetPtEtaPhiM(LepPt->at(0), LepEta->at(0), LepPhi->at(0), 0);
         l2->SetPtEtaPhiM(LepPt->at(1), LepEta->at(1), LepPhi->at(1), 0);
         l3->SetPtEtaPhiM(LepPt->at(2), LepEta->at(2), LepPhi->at(2), 0);
         l4->SetPtEtaPhiM(LepPt->at(3), LepEta->at(3), LepPhi->at(3), 0);
-
 	*Z1 = *l1 + *l2;
         *Z2 = *l3 + *l4;
 
         *higgs = *Z1 + *Z2;
-        
+
 	if(Path=="ggH125")
-		histoSig->Fill(higgs->M(),w);
+		
+		{
+			signalDis= 1 / (1 + p_QQB_BKG_MCFM / p_GG_SIG_ghg2_1_ghz1_1_JHUGen);
+			histoSig->Fill(signalDis,w);
+			signal->Fill(higgs->M(),signalDis,w);
+		}	
 	else if(Path=="qqZZ")
-		histoBack->Fill(higgs->M(),w);
+		{
+			backgroundDis=1 / (1 + co * p_QQB_BKG_MCFM / p_GG_SIG_ghg2_1_ghz1_1_JHUGen);
+			histoBack->Fill(backgroundDis,w);
+			background->Fill(higgs->M(),backgroundDis,w);
+		}
 }
+c->cd(1);
 
-	histoBack->GetXaxis()->SetTitle("m_{4l} (GeV)");
-	histoBack->GetYaxis()->SetTitle("Events / 2GeV");
-	histoBack->SetStats(0);
-	histoBack->SetLineColor(kBlue);
-	histoBack->SetFillColor(kBlue);
-	hs->Add(histoBack);
-
-	histoSig->GetXaxis()->SetTitle("m_{4l} (GeV)");
-	histoSig->GetYaxis()->SetTitle("Events / 2GeV");
+	histoSig->Scale(1.0/histoSig->Integral());
+	histoSig->GetXaxis()->SetTitle("D_{kin}");
+	histoSig->GetYaxis()->SetTitle("Events / 0.02");
+	histoSig->GetYaxis()->SetRangeUser(0, 1);
 	histoSig->SetStats(0);
 	histoSig->SetLineColor(kRed);
-	histoSig->SetFillColor(kRed);
-	hs->Add(histoSig);
+	histoSig->SetLineWidth(3);
+	histoSig->Draw("hist");	
 
-	hs->Draw("hist");
-	hs->GetXaxis()->SetTitle("m_{4l} (GeV)");
-	hs->GetYaxis()->SetTitle("Events / 2GeV");
+	float scale=histoBack->Integral();
+	cout<<scale<<endl;
+	scale=4018.12;
+
+	histoBack->Scale(1.0/scale);
+	histoBack->GetXaxis()->SetTitle("D_{kin}");
+	histoBack->GetYaxis()->SetTitle("Events / 0.02");
+	histoBack->GetYaxis()->SetRangeUser(0, 1);
+	histoBack->SetStats(0);
+	histoBack->SetLineColor(kBlue);
+	histoBack->SetLineWidth(3);
+	histoBack->Draw("hist same");
+
+	for(int i=0; i<1000; i++)
+	{
+		double x=1.0-histoBack->Integral(1,i+1);
+		double y=1.0-histoSig->Integral(1,i+1);
+		if ( x > 0.001 && y > 0.001 && x < 1.0 && y < 1.0) 
+			ROC->SetPoint(i,x,y);
+		
+	}
 
 	TLegend* legend = new TLegend(0.7,0.8,0.9,0.9);
 	legend->SetTextSize(0.03);
-	legend->AddEntry(histoSig,"Signal","f");
-	legend->AddEntry(histoBack,"Background","f");
+	legend->AddEntry(histoSig,"Signal","l");
+	legend->AddEntry(histoBack,"Background","l");
 	legend->Draw();
 
-	c->SaveAs("Reconstructed_Higgs.png");
-}	
+	c->cd(2);   
+	
+	ROC->SetMinimum(0.95);
+	ROC->SetMaximum(1.0);
+	ROC->GetXaxis()->SetLimits(0.00,0.05);
+	ROC->GetXaxis()->SetTitle("Background");
+	ROC->GetYaxis()->SetTitle("Signal");
+	ROC->SetTitle("ROC curve");
+	ROC->Draw();
+	c->Update();	
+
+	c->cd(3);
+
+	background->SetTitle("Background");
+	background->GetXaxis()->SetTitle("m_{4l} (GeV)");
+	background->GetYaxis()->SetTitle("D_{kin}");
+	background->SetMinimum(-0.01);
+	background->SetStats(0);
+	background->Draw("COLZ");
+
+	c->cd(4);
+
+	signal->SetTitle("Signal");
+	signal->GetXaxis()->SetTitle("m_{4l} (GeV)");
+	signal->GetYaxis()->SetTitle("D_{kin}");
+	signal->SetMinimum(-0.01);
+	signal->SetStats(0);
+	signal->Draw("COLZ");
+
+	c->SaveAs("ROC.png");
+}
+
 
